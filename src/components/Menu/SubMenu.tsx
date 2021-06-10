97d40c0739b7fde878f96650e98d7ee6a2ac1f53
bensonnn7 <38306742+bensonnn7@users.noreply.github.com>
2021-06-10 07:51:55 -0700

    install basic icon

--- src/components/Menu/SubMenu.tsx
+++ src/components/Menu/SubMenu.tsx
@@ -48,7 +48,7 @@ const SubMenu: React.FC<SubMenuProps> = ({
     },
   };
   const renderChildren = () => {
-    const subMenuClasses = classNames("viking-submenu", {
+    const subMenuClasses = classNames("ovis-submenu", {
       "menu-opened": menuOpen,
     });
     const childrenComponent = React.Children.map(children, (child, index) => {
